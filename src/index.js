3c95396e74b03466ee0e323b72632135a888f690
Cecilia De Rosa <cec.derosa@gmail.com>
2026-02-12 14:05:44 -0700

    Added search function using api

--- src/index.js
+++ src/index.js
@@ -1,17 +1,46 @@
-function handleSearchSubmit1(event) {
+function refreshWeather1(response) {
+  let temperatureElement1 = document.querySelector("#temperature1");
+  let temperature1 = response.data.temperature.current;
+ let cityElement1=document.querySelector("#city1");
+  cityElement1.innerHTML= response.data.city;
+  temperatureElement1.innerHTML = Math.round(temperature1);
+}
+
+function searchCity1(city1) {
+let apiKey ="c7o85b9t9db5f04fdbe341094a1feefd";
+let apiUrl1 = `https://api.shecodes.io/weather/v1/current?query=${city1}&key=${apiKey}&units=metric`;
+console.log(apiUrl1);
+axios.get(apiUrl1).then(refreshWeather1);
+}
+ function handleSearchSubmit1(event) {
     event.preventDefault();
     let searchInput1 = document.querySelector("#search-form-input-1");
-  let cityElement1=document.querySelector("#city1");
-  cityElement1.innerHTML= searchInput1.value;
+ 
+  searchCity1(searchInput1.value);
 
+} 
+function refreshWeather2(response) {
+ let temperatureElement2 = document.querySelector("#temperature2");
+  let temperature2 = response.data.temperature.current;
+ let cityElement2=document.querySelector("#city2");
+  cityElement2.innerHTML= response.data.city;
+  temperatureElement2.innerHTML = Math.round(temperature2);
 }
+
+function searchCity2(city2) {
+let apiKey ="c7o85b9t9db5f04fdbe341094a1feefd";
+let apiUrl2 = `https://api.shecodes.io/weather/v1/current?query=${city2}&key=${apiKey}&units=metric`;
+console.log(apiUrl2);
+axios.get(apiUrl2).then(refreshWeather2);
+}
+
 
 function handleSearchSubmit2(event) {
     event.preventDefault();
 
    let searchInput2 = document.querySelector("#search-form-input-2");
-  let cityElement2=document.querySelector("#city2");
-  cityElement2.innerHTML= searchInput2.value;
+
+  searchCity2(searchInput2.value);
 }
 
 let searchFormElement1 = document.querySelector("#search-form-1");
